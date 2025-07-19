5c4b2a0df52ad53297cc2b75ae0b010955cab802
AkshayKumar2211 <taara0361@gmail.com>
2025-07-19 22:16:50 +0530

    "Learning Repository pattren after this"

--- Villa_project.Infrastructure/Data/ApplicationDbContext.cs
+++ Villa_project.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace Villa_project.Infrastructure.Data
 
         public DbSet<Villa> Villas { get; set; }
 
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // base.OnModelCreating(modelBuilder);
@@ -50,6 +52,35 @@ namespace Villa_project.Infrastructure.Data
                     Sqft=5000,
                     Occupancy=6,
                     ImageUrl="https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmlsbGF8ZW58MHx8MHx8fDA%3D"
+                }
+               
+                );
+
+            modelBuilder.Entity<VillaNumber>().HasData(
+                new VillaNumber
+                {
+                    Villa_Number=101,
+                    VillaId=7
+                },
+                new VillaNumber
+                {
+                    Villa_Number=102,
+                    VillaId=7
+                },
+                new VillaNumber
+                {
+                    Villa_Number=103,
+                    VillaId=8
+                },
+                new VillaNumber
+                {
+                    Villa_Number=104,
+                    VillaId=7
+                },
+                new VillaNumber
+                {
+                    Villa_Number=105,
+                    VillaId=8
                 }
                 );
         }
---
+++ Villa_project/Controllers/VillaNumberController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using Villa_project.Domain.Entities;
+using Villa_project.Infrastructure.Data;
+using Villa_project.ViewModels;
+
+namespace Villa_project.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            var villasNumberInDb = _db.VillaNumbers.Include(u=>u.Villa).ToList();
+            return View(villasNumberInDb);
+        }
+
+        public IActionResult Create()
+        {
+            /* IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(
+                 u => new SelectListItem
+                 {
+                     Text = u.Name, // this will be options in drop down
+                     Value=u.Id.ToString()
+                 }
+                 );
+
+             ViewData["Villa List"]=list;*/
+
+            VillaNumberVM villaNumberVM = new()
+            {
+                Villalist=_db.Villas.ToList().Select(u => new SelectListItem
+                {
+                    Text=u.Name,
+                    Value=u.Id.ToString()
+                }
+               )
+            };
+            return View(villaNumberVM);
+      
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumberVM villaNumber)
+        {
+            bool exist = _db.VillaNumbers.Any(u => u.Villa_Number==villaNumber.VillaNumber.Villa_Number);
+            if (exist)
+            {
+                TempData["error"]="Data is already in database";
+
+            }
+            if (villaNumber == null) return BadRequest();
+
+             ModelState.Remove("Villa");
+             if (ModelState.IsValid)
+             {
+                 _db.VillaNumbers.Add(villaNumber.VillaNumber);
+                 _db.SaveChanges();
+                 return RedirectToAction("Index");
+             }
+             return View(villaNumber);
+           
+        }
+    }
+}
